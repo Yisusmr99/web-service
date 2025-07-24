ccb2976287ab9c16a3486b080070025cb98f812d
Dervin2212 <dalvaradop4@miumg.edu.gt>
2025-07-24 10:34:25 -0600

    testa automatizado y QA

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,31 @@
 const pool = require('../config/db');
 
-/*async function getAllUsers() {
+async function getAllUsers() {
+  try {
     const [rows] = await pool.query('SELECT * FROM users');
     return rows;
-}
-    */
-
-async function getAllUsers() {
-    try {
-        const [rows] = await pool.query('SELECT * FROM users');
-        return rows;
-    } catch (err) {
-        console.error('Error en getAllUsers:', err);
-        throw err; // it launches it again to reach the controller
-    }
+  } catch (err) {
+    console.error('❌ Error en getAllUsers:', err);
+    throw err;
+  }
 }
 
 async function createUser(first_name, last_name, email, pass) {
+  try {
     const [result] = await pool.query(
-        'INSERT INTO users (first_name, last_name, email, pass) VALUES (?, ?, ?, ?)',
-        [first_name, last_name, email, pass]
+      'INSERT INTO users (first_name, last_name, email, pass) VALUES (?, ?, ?, ?)',
+      [first_name, last_name, email, pass]
     );
     return { user_id: result.insertId, first_name, last_name, email };
+  } catch (err) {
+    console.error('❌ Error en createUser:', err);
+    if (err.code === 'ER_DUP_ENTRY') {
+      const error = new Error('El correo ya está registrado');
+      error.status = 409;
+      throw error;
+    }
+    throw err;
+  }
 }
 
 module.exports = { getAllUsers, createUser };
